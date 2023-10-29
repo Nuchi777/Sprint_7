6a936c518fd818b3feb099bcd3a28d2462fe4982
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-10-29 15:48:32 +0300

    added test_create_couriers_fill_not_all_required_field

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -1,4 +1,6 @@
 import json
+
+import pytest
 import requests
 
 from register_new_courier import register_new_courier_and_return_login_password
@@ -44,8 +46,8 @@ class TestCreateCourier:
         # логинимся под созданным курьером
         endpoint_login_courier = '/api/v1/courier/login'
         data_login = {
-            "login": f"{login}",
-            "password": f"{password}"
+            "login": login,
+            "password": password
         }
         response = requests.post(f'{url}{endpoint_login_courier}', data=data_login)
         id_courier = response.json()['id']
@@ -54,9 +56,9 @@ class TestCreateCourier:
         # создаем курьера с теми же параметрами
         endpoint_create_courier = '/api/v1/courier'
         data_create = {
-            "login": f"{login}",
-            "password": f"{password}",
-            "firstName": f"{firstName}"
+            "login": login,
+            "password": password,
+            "firstName": firstName
         }
         response = requests.post(f'{url}{endpoint_create_courier}', data=data_create)
         assert response.status_code == 409 and response.text == '{"message": "Этот логин уже используется"}'
@@ -65,6 +67,19 @@ class TestCreateCourier:
         endpoint_delete_courier = f'/api/v1/courier/{id_courier}'
         response = requests.delete(f'{url}{endpoint_delete_courier}')
         assert response.status_code == 200
+
+    @pytest.mark.parametrize("login, password", [["", "12345"], ["e.koloskov", ""]])
+    def test_create_couriers_fill_not_all_required_field(self, login, password):
+
+        # создаем курьера без логина или пароля
+        endpoint_create_courier = '/api/v1/courier'
+        data_create = {
+            "login": login,
+            "password": password
+        }
+        response = requests.post(f'{url}{endpoint_create_courier}', data=data_create)
+        assert response.status_code == 400 and response.text == '{"message": "Недостаточно данных для создания учетной записи"}'
+
 
 
 
